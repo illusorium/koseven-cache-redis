bbd5bfa1f4a8133980f39ab792675021c6a6acac
Alexander Danilin <illusorium@gmail.com>
2019-08-02 16:12:46 +0300

    init

---
+++ classes/Kohana/Cache/Redis.php
@@ -0,0 +1,40 @@
+<?php
+
+use Predis\Client;
+
+class Kohana_Cache_Redis extends Cache
+{
+    protected $redisClient;
+
+    protected function __construct(array $config)
+    {
+        parent::__construct($config);
+
+        $connectionParams = Arr::get($config, 'connection');
+        $this->redisClient = new Client($connectionParams);
+    }
+
+    public function get($id, $default = null)
+	{
+	    if ($this->redisClient->exists($id)) {
+            $data = $this->redisClient->get($id);
+            return unserialize($data);
+        }
+	    return $default;
+	}
+
+	public function set($id, $data, $lifetime = null)
+	{
+        return $this->redisClient->setex($id, $lifetime ?: Cache::DEFAULT_EXPIRE, serialize($data));
+	}
+
+	public function delete($id)
+	{
+	    return $this->redisClient->del((array) $id);
+	}
+
+    public function delete_all()
+    {
+        return $this->redisClient->flushall();
+    }
+}
---
+++ config/cache.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'redis' => [
+        'driver' => 'redis',
+        'connection' => [
+            'host' => '127.0.0.1',
+            'port' => 6379
+        ]
+    ]
+];
