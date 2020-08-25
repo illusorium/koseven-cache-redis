c0e23f788d8c76d8a8ecc77304b81a35f180272b
illusorium <illusorium@gmail.com>
2020-08-25 17:43:54 +0300

    add methods to work with sets

--- classes/Kohana/Cache/Redis.php
+++ classes/Kohana/Cache/Redis.php
@@ -15,15 +15,15 @@ class Kohana_Cache_Redis extends Cache
     }
 
     public function get($id, $default = null)
-	{
-	    if ($this->redisClient->exists($id)) {
+    {
+        if ($this->redisClient->exists($id)) {
             $data = $this->redisClient->get($id);
             return unserialize($data);
         }
-	    return $default;
-	}
+        return $default;
+    }
 
-	public function hget($key, $field, $default = null)
+    public function hget($key, $field, $default = null)
     {
         if ($this->redisClient->hexists($key, $field)) {
             $data = $this->redisClient->hget($key, $field);
@@ -32,13 +32,13 @@ class Kohana_Cache_Redis extends Cache
         return $default;
     }
 
-	public function set($id, $data, $lifetime = null)
-	{
+    public function set($id, $data, $lifetime = null)
+    {
         if (null === $lifetime) {
             $lifetime = $this->config('default_expire');
         }
         return $this->redisClient->setex($id, $lifetime ?: Cache::DEFAULT_EXPIRE, serialize($data));
-	}
+    }
 
     public function hset($key, $field, $data, $lifetime = null)
     {
@@ -53,10 +53,42 @@ class Kohana_Cache_Redis extends Cache
         return $hsetResult;
     }
 
-	public function delete($id)
-	{
-	    return $this->redisClient->del((array) $id);
-	}
+    public function sadd($key, $data, $lifetime = null)
+    {
+        $exists = $this->redisClient->exists($key);
+        if (null === $lifetime) {
+            $lifetime = $this->config('default_expire');
+        }
+        $saddResult = $this->redisClient->sadd($key, (array) $data);
+        if (!$exists) {
+            $this->redisClient->expire($key, $lifetime ?: Cache::DEFAULT_EXPIRE);
+        }
+        return $saddResult;
+    }
+
+    public function srem($key, $value) {
+        return $this->redisClient->srem($key, $value);
+    }
+
+    public function smembers($key)
+    {
+        return $this->redisClient->smembers($key);
+    }
+
+    public function sismember($key, $value)
+    {
+        return $this->redisClient->sismember($key, $value);
+    }
+
+    public function rename($oldKey, $newKey)
+    {
+        return $this->redisClient->rename($oldKey, $newKey);
+    }
+
+    public function delete($id)
+    {
+        return $this->redisClient->del((array) $id);
+    }
 
     public function delete_all()
     {
